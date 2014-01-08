093f0713655a0531a4769e9c948391973374b79c
Bahadur Oad <bahadur.oad@gmail.com>
2014-01-08 19:02:16 +0500

    Worked from office on 8th Jan 2014

---
+++ new.php
@@ -0,0 +1,23 @@
+<html>
+    
+    <head>
+        <title>
+            This is test
+        </title>
+    </head>
+    <body>
+        
+        
+        <?php
+        echo "<h1> Test Page</h1>
+        <p>  This page is for testing only. yes this is working</p>";
+         echo "<h1> Test Page</h1>
+        <p>  This page is for testing only. yes this is working</p>";
+         echo "<h1> Test Page</h1>
+        <p>  This page is for testing only. yes this is working</p>";
+        ?>
+        
+        
+    </body>
+</html>
+
